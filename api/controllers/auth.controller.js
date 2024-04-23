7d6b2b3ec297350b864718a5279de9ae7c5ecc5a
Anshul <Anshul@anshul.com>
2024-04-23 22:48:37 +0530

    completed sign in

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -23,7 +23,7 @@ export const signin = async (req, res, next) => {
   try {
     const validUser = await User.findOne({ email });
     if (!validUser) return next(errorHandler(404, "Invalid Credentials."));
-    const validPassword = bcryptjs.hashSync(password, validUser.password);
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) return next(errorHandler(404, "Invalid Credentials."));
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
     const { password: pass, ...userInfo } = validUser._doc;
