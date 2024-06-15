0ddbba4737e8f255b5c982cd66fee18bc1e03f2f
Anshul <Anshul@anshul.com>
2024-06-15 14:09:23 +0530

    create update user api

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 dotenv.config("");
 
 mongoose
@@ -15,6 +16,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/auth", authRouter);
